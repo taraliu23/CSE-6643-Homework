fdb11b61946fea1267fcba900f44e09c9f11bd75
taraliu23 <rainylty@gmail.com>
2025-03-15 14:31:42 -0400

    init: hw5

--- HW4_code/HW4_your_code.jl
+++ HW4_code/HW4_your_code.jl
@@ -6,22 +6,41 @@
 # It should not modify A
 function classical_gram_schmidt(A)
     # YOUR CODE HERE
+    #=
+
+    input:  matrix A
+    output: matrices Q and R
+
+    params: 
+    m, n = size(A)
+    Q = similar(A, m, n)
+     R = zeros(n, n)
+
+    process: classical gram-schmidt algorithm
+    =#
 
     m, n = size(A)
 
     Q = similar(A, m, n)
     R = zeros(n, n)
 
+    # loop through columns of A
     for j in 1:n
+
         v = A[:, j]
+
+        # loop through columns of Q
         for i in 1:j-1
-            R[i, j] = dot(Q[:, i], A[:, j])
-            v -= R[i, j] * Q[:, i]
+            R[i, j] = sum(Q[:, i] .* A[:, j])
+
+            v .-= R[i, j] .* Q[:, i]
+
         end
+
         R[j, j] = norm(v)
         Q[:, j] = v / R[j, j]
-    end
 
+    end
 
 
     return Q, R
@@ -36,7 +55,38 @@ end
 function modified_gram_schmidt(A)
     # YOUR CODE HERE
 
+    #=
 
+    input:  matrix A
+    output: matrices Q and R
+
+    params: 
+    m, n = size(A)
+    Q = similar(A, m, n)
+    R = zeros(n, n)
+
+    process: 
+
+
+    =#
+
+    m, n = size(A)
+    Q = deepcopy(A)
+    R = zeros(n, n)
+
+    for j in 1:n
+
+        R[j, j] = norm(Q[:, j])
+
+        Q[:, j] ./= R[j, j]
+
+        for i in (j+1):n
+
+            R[j, i] = sum(Q[:, j] .* Q[:, i])
+
+            Q[:, i] .-= R[j, i] .* Q[:, j]
+        end
+    end
 
     return Q, R
 end
@@ -50,6 +100,7 @@ end
 # It should not allocate any memory.  
 function householder_QR!(A)
     # YOUR CODE HERE
+
 end
 
 #----------------------------------------
@@ -63,8 +114,10 @@ end
 # use the preallocated output vector to record the result. 
 function householder_QR_mul!(out, x, QR)
     # YOUR CODE HERE
+
 end
 
 function householder_QR_div!(out, b, QR)
     # YOUR CODE HERE
+
 end
